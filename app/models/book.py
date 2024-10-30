a518b3d4cf2898909b9c420b93532d12e48138b3
anhtran <anhtr077@gmail.com>
2024-10-29 19:39:03 -0700

    Update database and run migration

--- app/models/book.py
+++ app/models/book.py
@@ -6,17 +6,10 @@ class Book(db.Model):
     title: Mapped[str]
     description: Mapped[str]
 
-def to_dict(self):
-    return dict(
-        id=self.id,
-        title=self.title,
-        description=self.description    
-    )
-
-        
-# books = [
-#     Book(1, "The Gifts of Imperfection", "Encourages readers to be kinder to themselves, and to accept imperfections"),
-#     Book(2, "You Are a Badass", "Overcome doubts and fears that hold them back from living the life they desire"),
-#     Book(3, "Essentialism: The Disciplined Pursuit of Less", "Say no to unnecessary obligations, and reclaim your time for truly important thing")
-# ]
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            description=self.description    
+        )
 
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from ..db import db
 from app.models.book import Book
 
@@ -7,13 +7,12 @@ book_bp = Blueprint("book_bp", __name__, url_prefix="/books")
 @book_bp.post("")
 def create_book():
     request_body = request.get_json()
-    id = request_body["id"]
     title = request_body["title"]
     description = request_body["description"]
     
-    new_book = Book(id=id, title=title, description=description)
-    db.session.add(new_book)
-    db.session.commit()
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book) 
+    db.session.commit()# until we do this commit, the row will be added to db and show record
     
     response = new_book.to_dict()
     return response, 201
@@ -26,18 +25,53 @@ def get_all_books():
     
     return response_body, 200
     
-
-
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except ValueError:
-#         response = {"message": f"book {book_id} invalid"}
-#         abort(make_response(response, 400))
+@book_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
     
-#     for book in books:
-#         if book.id == book_id:
-#             return book
+    return book.to_dict()
+
+@book_bp.put("/<book_id>")
+def update_one_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
     
-#     response = {"message": f"book {book_id} not found"}
-#     abort(make_response(response, 404))
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+    
+    return Response(status=204, mimetype="application/json")
+    #using Response constructor
+
+@book_bp.delete("/<book_id>")
+def delete_one_book(book_id):
+    book = validate_book(book_id)
+    
+    db.session.delete(book) # live connection to database, I want to add, update, delete 
+    
+    db.session.commit()# transaction, allow group all changes happens together or not at all
+    
+    return Response(status=204, mimetype="application/json")
+    
+    
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+    
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query) #returns a ScalarResult object instead of an actual Book object 
+    # book = db.session.execute(query).scalars().first() 
+    #sequence will retrieve the Book object itself instead of returning a ScalarResult object. 
+    #.scalars() extracts the actual model objects from the result, 
+    # and .first() retrieves the first result (or None if there are no matches).
+    
+    if not book:
+        response = {"message": f"book {book_id} not found"} 
+        abort(make_response(response, 404))
+        
+    return book
+    
